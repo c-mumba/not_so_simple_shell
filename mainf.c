63de630f000c5b18ab4e7c5f2567acc434a0704d
Christian <macheadway@gmail.com>
2023-05-24 20:21:48 -0700

    try man

---
+++ mainf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main- Short description, single line
+ * @argc: Description of parameter x
+(* @argv: Description of parameter x
+ * @env: Description of parameter x
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(int argc, char *argv[], char *env[])
+{
+	inf_loop(env);
+	if (!argc && !argv)
+	return (0);
+	else
+	return (0);
+}
